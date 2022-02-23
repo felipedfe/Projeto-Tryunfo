c7a524ad362eee8752393803638c721c9b4c6b0e
Felipe Fernandes <felipedfe@gmail.com>
2022-02-23 17:24:51 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -7,18 +8,28 @@ class App extends React.Component {
       <div>
         <h1>Tryunfo</h1>
         <Form
-          cardName=" "
-          cardDescription=" "
+          cardName="Tartaruga"
+          cardDescription="Um animal bem lento."
           cardAttr1="2"
-          cardAttr2=" "
-          cardAttr3=" "
-          cardImage=" "
-          cardRare=" "
+          cardAttr2="3"
+          cardAttr3="5"
+          cardImage="tartaruga.jpg"
+          cardRare="raro"
           cardTrunfo={ false }
           hasTrunfo={ false }
           isSaveButtonDisabled={ false }
           onInputChange={ () => {} }
           onSaveButtonClick={ () => {} }
+        />
+        <Card
+          cardName="Pinguim"
+          cardDescription="Um animal simpático."
+          cardAttr1="2"
+          cardAttr2="7"
+          cardAttr3="9"
+          cardImage="pinguim.jpg"
+          cardRare="raro"
+          cardTrunfo
         />
       </div>
     );
